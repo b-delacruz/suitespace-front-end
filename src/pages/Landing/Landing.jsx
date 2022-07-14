4a92d97f691e830847430030e4c827ea91bd7733
andywtse <atse4995@email.com>
2022-07-14 15:46:20 -0400

    Adds tailwind css

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,6 +4,9 @@ const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Hello world!
+      </h1>
     </main>
   )
 }
