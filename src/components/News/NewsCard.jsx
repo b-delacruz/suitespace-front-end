4ba0936d9385b93e133ac61adc04024259a9ed67
b-delacruz <brianmdelacruz@gmail.com>
2022-07-20 18:25:07 -0400

    fix bugs

--- src/components/News/NewsCard.jsx
+++ src/components/News/NewsCard.jsx
@@ -1,30 +1,26 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import './NewsCard.css'
 
 export default function NewsCard(props) {
-
 
   return (
     <Card sx={{ display: 'flex' }}>
       <CardMedia
         component="img"
-        sx={{ width: 125 }}
+        sx={{ width: 125, height: 200, margin:'auto' }}
         image={props.news.urlToImage ? props.news.urlToImage : 'https://i.imgur.com/QoMTW46.jpg' }
         alt="News image"
       />
       <div className='news-card-container | flex flex-col justify-start items-start gap-2 '>
-        <div className='news-card-header | text-xl'>
-          {props.news.title
-            .split(".").splice(0, 1).join(" ")
-          }
+        <div className='news-card-header | font-bold'>
+            {props.news.title.split(' ').slice().join(' ')}
         </div>
         <div className='news-card-description | flex justify-start'>
-          {props.news.description.split(".").splice(0 , 1).join(" ")}
+    
+            {props.news.description.split('.').slice(0,4).join('.')}
+        
         </div>
         <a target="_blank" href={props.news.url}>
           <button className='news-button | flex justify-center items-center text-base rounded px-5 py-1 '>
--- src/pages/NewsFeed/NewsFeed.jsx
+++ src/pages/NewsFeed/NewsFeed.jsx
@@ -45,25 +45,13 @@ const NewsFeed = (props) => {
     return !(search)
   }
 
-  const getFontSize = (textLength) => {
-    const baseSize = 8
-    if (textLength >= baseSize) {
-      textLength = baseSize - 1
-    }
-    const fontSize = baseSize - textLength
-    return `${fontSize}vw`
-  }
-  
-  const boxes = document.querySelectorAll('.box p')
-    
-  boxes.forEach(box => {
-    box.style.fontSize = getFontSize(box.textContent.length)
-  })
   return (
     <>
       <div className="news-container">
         <div className="news-nav | flex justify-between items-center">
-          <h1 className='news-title | text-xl'>News Feed</h1> 
+          <div className="output | break-all break-word">
+            <h1 className='news-title | text-xl'>News Feed</h1> 
+          </div>
           <form 
           autoComplete="off" 
           onSubmit={handleSubmit}
