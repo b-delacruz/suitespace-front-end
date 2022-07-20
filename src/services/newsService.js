5ad72779ff84c3ffc65137b2ce5b5fcbd46ddd73
b-delacruz <brianmdelacruz@gmail.com>
2022-07-20 11:50:39 -0400

    merge main

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,11 +1,3 @@
-// function getNews(search) {
-//   return fetch(`/api/news/${search}`)
-//   .then(res => res.json())
-// }
-
-// export {
-//   getNews
-// }
 const baseUrl = `${process.env.REACT_APP_BACK_END_SERVER_URL}`
 
 export async function getNews (search) {
